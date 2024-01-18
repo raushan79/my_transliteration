dd473b3d4f0185913746d490fd2dfdce7a083b98
Raushan Kumar <raushan@crimecheck.in>
2024-01-18 22:58:55 +0530

    mh trans script added

--- start.sh
+++ start.sh
@@ -3,4 +3,8 @@
 pm2 start script/cg_arrest_trans.js --name "CG_Arrest_Trans$1" --log-date-format "YYYY-MM-DD HH:mm:ss.SSS" 
 
 
+# For MH arrest data
+pm2 start script/mh_arrest_trans.js --name "MH_Arrest_Trans$1" --log-date-format "YYYY-MM-DD HH:mm:ss.SSS" 
+
+
 # --cron "*/35 * * * *"
